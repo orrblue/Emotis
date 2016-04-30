00d3f859c64e2468caddf80bd8ac14fc0e99c353
Joshua Fan <joshuaptfan@gmail.com>
2016-04-30 15:33:16 -0700

    Don't submit if text blank

--- emotis-ajax.js
+++ emotis-ajax.js
@@ -1,7 +1,3 @@
-$(document).ready(function () {
-    $("#chatbox_input").focus();
-});
-
 // Fade out banner on first interaction
 $("*").one("click", function () {
     $("#banner").fadeOut("slow");
@@ -11,27 +7,29 @@ function submit_chatbox() {
     var text = $("#chatbox_input").val();
     var color = $("#chatbox_color").val();
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            $.ajax({
-                type: "POST",
-                url: "submit_chat.php",
-                data: {
-                    text: text,
-                    color: color,
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                },
-            }).done(function ( msg ) {
-                alert("[" + msg + "]");
-                $("#chatbox_input").val() = "";
-            }).fail(function () {
-                alert("Unable to send data to server");
+    if (text != "") {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                $.ajax({
+                    type: "POST",
+                    url: "submit_chat.php",
+                    data: {
+                        text: text,
+                        color: color,
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    },
+                }).done(function ( msg ) {
+                    alert("[" + msg + "]");
+                    $("#chatbox_input").val() = "";
+                }).fail(function () {
+                    alert("Unable to send data to server");
+                });
+            }, function ( error ) {
+                alert("Failed to get geographic location: " + error.code + ": " + error.message);
             });
-        }, function ( error ) {
-            alert("Failed to get geographic location: " + error.code + ": " + error.message);
-        });
-    } else {
-        alert("Browser does not support geolocation API");
+        } else {
+            alert("Browser does not support geolocation API");
+        }
     }
 }
